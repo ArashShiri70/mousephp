5fae60f99c9ee606efe4966169b157fee6ad78dc
unknown <a.shiri@netafraz.com>
2025-04-13 10:31:37 +0330

    Fix: change index file to use Request class

--- public/index.php
+++ public/index.php
@@ -1,6 +1,18 @@
 <?php
 
-require_once __DIR__ . '/../core/App.php';
+require_once __DIR__ . '/../routes/web.php';
+require_once __DIR__ . '/../core/Route.php';
+require_once __DIR__ . '/../core/Request.php';
 
-$app = new App();
-$app->run();
+
+use Core\Route;
+use Core\Request;
+
+$request = new Request();
+
+/**
+ * ✅ DIP (Dependency Inversion Principle):
+ * Route should not create its own Request object.
+ * Instead, the main file injects dependencies.
+ */
+Route::resolve($request->getPath(), $request->getMethod());
