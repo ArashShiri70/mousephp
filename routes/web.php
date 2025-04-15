316eeb311ef101cc643bc58187fda760da5e36fa
unknown <a.shiri@netafraz.com>
2025-04-15 16:05:59 +0330

    Feat: register post route added

--- routes/web.php
+++ routes/web.php
@@ -2,8 +2,12 @@
 
 require_once __DIR__ . '/../core/Route.php';
 require_once __DIR__ . '/../app/controllers/HomeController.php';
+require_once __DIR__ . '/../app/controllers/Auth/UserController.php';
 
 use core\Route;
 use app\controllers\HomeController;
+use app\controllers\auth\UserController;
 
 Route::get('/', [HomeController::class, 'index']);
+
+Route::post('/register', [UserController::class, 'register']);
